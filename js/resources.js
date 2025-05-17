3c522fef984e28e43f15070d4053af5955b9788b
JohnPorksBrotherInLaw <cablaoandrei490@gmail.com>
2025-05-17 06:36:33 -0600

    Initial commit
    Changes to be committed:
    new file:   data/img/Blank_Sprite_Sheet_4_2_by_KnightYamato.png
    	new file:   data/img/testTileMap.png
    	new file:   data/map/testMap.tmx
    	new file:   index.css
    	new file:   index.html
    	new file:   js/entities/entities.js
    	new file:   js/index.js
    	new file:   js/resources.js
    	new file:   js/screens/play.js

---
+++ js/resources.js
@@ -0,0 +1,34 @@
+var resources = [
+
+    /* Graphics.
+     * @example
+     * { name: "example", type:"image", src: "data/img/example.png" },
+     */
+    { name: "testTileMap",  type:"image", src: "data/img/testTileMap.png" },  
+    { name: "Blank_Sprite_Sheet", type:"image", src: "data/img/Blank_Sprite_Sheet_4_2_by_KnightYamato.png" },
+
+    /* Maps.
+     * @example
+     * { name: "example01", type: "tmx", src: "data/map/example01.tmx" },
+     * { name: "example01", type: "tmx", src: "data/map/example01.json" },
+      */
+    { name: "testMap", type: "tmx", src: "data/map/testMap.tmx" },
+
+    /* Background music.
+     * @example
+     * { name: "example_bgm", type: "audio", src: "data/bgm/" },
+     */
+
+    /* Sound effects.
+     * @example
+     * { name: "example_sfx", type: "audio", src: "data/sfx/" }
+     */
+
+    /* Atlases
+     * @example
+     * { name: "example_tps", type: "json", src: "data/img/example_tps.json" },
+     */
+
+];
+
+export default resources;
