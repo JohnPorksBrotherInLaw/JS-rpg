e9fcbfd3c3c5afb9af8e0c2f8f764e822b4fbd28
JohnPorksBrotherInLaw <cablaoandrei490@gmail.com>
2026-01-03 07:09:24 -0700

    added more graphics

--- js/screens/play.js
+++ js/screens/play.js
@@ -21,6 +21,20 @@ class PlayScreen extends me.Stage {
         me.pool.register("DOOR_ClassroomBHallway",Ent.Door,true);
         me.pool.register("DOOR_ClassroomC",Ent.Door,true);
         me.pool.register("DOOR_ClassroomCHallway",Ent.Door,true);
+        me.pool.register("DOOR_Caf",Ent.Door,true);
+        me.pool.register("DOOR_CafHallway",Ent.Door,true);
+        me.pool.register("DOOR_Infirmary",Ent.Door,true);
+        me.pool.register("DOOR_Library",Ent.Door,true);
+        me.pool.register("DOOR_Storage",Ent.Door,true);
+        me.pool.register("DOOR_Kitchen",Ent.Door,true);
+        me.pool.register("DOOR_DormA",Ent.Door,true);
+        me.pool.register("DOOR_DormB",Ent.Door,true);
+        me.pool.register("DOOR_DormC",Ent.Door,true);
+        me.pool.register("DOOR_DormD",Ent.Door,true);
+        me.pool.register("DOOR_DormE",Ent.Door,true);
+        me.pool.register("DOOR_DormF",Ent.Door,true);
+        me.pool.register("DOOR_DormG",Ent.Door,true);
+        me.pool.register("DOOR_DormH",Ent.Door,true);
         // load a level
         me.level.load("floor1");
         
